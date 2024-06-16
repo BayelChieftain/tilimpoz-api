c9370b5ed9ba189d76cc41845e9e3aa728bfad0b
Chyngyz <konuratov.chika@gmail.com>
2024-06-16 16:44:07 +0600

    Created team model

--- tilimpoz/tilimpoz/urls.py
+++ tilimpoz/tilimpoz/urls.py
@@ -1,10 +1,12 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
-    path('', RedirectView.as_view(url='/api/', permanent=True)),
-]
-
+                  path('admin/', admin.site.urls),
+                  path('api/', include('core.urls')),
+                  path('api/team/', include('team.urls')),
+                  path('', RedirectView.as_view(url='/api/', permanent=True)),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
